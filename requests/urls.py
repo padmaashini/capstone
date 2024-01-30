57ef289091ac0e35a3c49c1676c917e5a4d63113
Padmaashini Sukumaran <padmaashini.s@gmail.com>
2024-01-29 20:46:42 -0500

    Add route for practitioner requests

--- requests/urls.py
+++ requests/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from requests import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path('', views.home, name='home'),
+    path('practitioner/<int:id>/requests', views.requests_for_practitioner, name="requests_for_practitioner")
 ]
--- requests/views.py
+++ requests/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def home(request):
     return render(request, "pages/home.html", {})
+
+def requests_for_practitioner(request, id):
+    print('id', id)
+    data = []
+    
+    return JsonResponse({ "status": "Success", "data": data })
